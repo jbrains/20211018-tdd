b3b28d488f98b6ff719073f6dec237620209eabf
J. B. Rainsberger <me@jbrains.ca>
2021-10-19 11:05:52 -0300

    Removed redundant special cases.

--- app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
+++ app/src/test/java/ca/jbrains/math/test/AddFractionsTest.java
@@ -66,14 +66,9 @@ public class AddFractionsTest {
         }
 
         public Fraction plus(Fraction that) {
-            if (this.denominator == 1 || that.denominator == 1)
-                return new Fraction(this.numerator + that.numerator);
-            else if (this.denominator == that.denominator)
-                return new Fraction(this.numerator + that.numerator, this.denominator);
-            else
-                return new Fraction(
-                        this.numerator * that.denominator + that.numerator * this.denominator,
-                        this.denominator * that.denominator);
+            return new Fraction(
+                    this.numerator * that.denominator + that.numerator * this.denominator,
+                    this.denominator * that.denominator);
         }
 
         @Override
