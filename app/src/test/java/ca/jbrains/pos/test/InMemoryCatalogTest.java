464e1f148912c2a14045ed9ed76e5731131a2bbe
J. B. Rainsberger <me@jbrains.ca>
2021-10-22 12:17:24 -0300

    Completed the contract tests for the Catalog.

--- app/src/test/java/ca/jbrains/pos/test/InMemoryCatalogTest.java
+++ app/src/test/java/ca/jbrains/pos/test/InMemoryCatalogTest.java
@@ -6,15 +6,35 @@ import org.junit.jupiter.api.Test;
 import java.util.HashMap;
 
 public class InMemoryCatalogTest {
+    // These tests refer only to the _contract_ of Catalog, but not
+    // a specific implementation.
 
     @Test
     void productFound() {
-        // This test refers only to the _contract_ of Catalog, but not
-        // a specific implementation.
         Assertions.assertEquals(
                 "EUR 7.95",
                 catalogWith("12345", "EUR 7.95")
                         .findPrice("12345"));
+    }
+
+    @Test
+    void productNotFound() {
+        Assertions.assertEquals(
+                null,
+                catalogWithout("12345").findPrice("12345"));
+    }
+
+    // Only these "factory methods" care about the specific implementation of
+    // Catalog. We can separate the factory methods from the contract tests
+    // into separate classes, either using inheritance or a combination of
+    // the Parameterized Test Case Pattern + Abstract Factory Pattern.
+    // (The exercise is left to you to try. :) )
+    private Catalog catalogWithout(String barcodeToAvoid) {
+        return new InMemoryCatalog(new HashMap<>() {{
+            put("not " + barcodeToAvoid, "::invalid price::");
+            put("also not " + barcodeToAvoid, "::invalid price::");
+            put("this is not " + barcodeToAvoid + ", you idiot", "::invalid price::");
+        }});
     }
 
     private Catalog catalogWith(String barcode, String matchingPrice) {
